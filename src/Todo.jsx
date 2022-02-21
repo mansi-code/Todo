ef7440c7738927c085e662402052873cb9d839fd
Mansi Jain <mansijain2225@gmail.com>
2022-02-21 21:10:40 +0530

    delete and edit functionality added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -7,22 +7,55 @@ import './Todo.css';
 const Todo = () =>{
     const uniqueId= uuid();
     const smallId= uniqueId.slice(0,8);
-    console.log(uniqueId);
+   
     const[data,setData]=useState([]);
     const[inputData,setInputData]=useState('');
     const handleCHange=(e) =>{
         setInputData(e.target.value);
-        console.log(inputData);
     }
 
+    const[toggle,setToggle]= useState(false);
+    const[editItemId, seteditItemId]= useState(null);
     const addItem =() =>{
         if(inputData===''){
             alert("enter something");
         }
-        else{
-        setData([...data,inputData]);
-        setInputData('');
+        else if(inputData &&toggle){
+            setData(
+                data.map((ele)=>{
+                    if(ele.id === editItemId){
+                        return {...ele,name :inputData}
+                    }
+                    return ele;
+                })
+            )
+            setToggle(false);
         }
+        else{
+        setData([...data,{
+            id: smallId,
+            name: inputData
+        }]);
+        }
+        setInputData('');
+    }
+
+    const deleteItem =(id)=>{
+        const dataAfterDeletion = data.filter((ele)=>{
+            return id!==ele.id
+        })
+        setData(dataAfterDeletion)
+    }
+
+    const editItem=(id) =>{
+        setToggle(true);
+        seteditItemId(id);
+        const previousValue =data.find(ele=> ele.id==id);
+        setInputData(previousValue.name);
+    }
+
+    const deleteAll=()=>{
+       setData([]);
     }
   return (
       <div className='Todo'>
@@ -37,7 +70,12 @@ const Todo = () =>{
                 <br/>
                 <br/>
                 <input type="text" placeholder='Enter your TODO' onChange={handleCHange} value={inputData}/>
-                <button className='addBtn' onClick={addItem}><i class="fa fa-plus" aria-hidden="true"></i></button>
+                {
+                    (toggle ===true) ?(<button className='inputEdit' onClick={addItem}><i class="fa fa-check-circle fa-lg" aria-hidden="true"></i></button>)
+                    :(<button className='addBtn' onClick={addItem}><i class="fa fa-plus" aria-hidden="true"></i></button>)
+                }
+                
+                
             </div>
             <br />
 
@@ -45,15 +83,21 @@ const Todo = () =>{
                 {
                     data.map((ele)=>{
                         return(
-                            <div className="eachItem">
-                            <span>{ele} </span>
-                            <button className='deleteBtn' onClick={deleteItem}><i class="fa fa-trash"></i></button>
+                            <div key={ele.id} className="eachItem">
+                            <span>{ele.name} </span>
+                            <button className='deleteBtn' onClick={()=> deleteItem(ele.id)}><i class="fa fa-trash"></i></button>
+                            <button className='editBtn' onClick={()=> editItem(ele.id)}><i class="fa fa-edit"></i></button>
                             </div>
                         )
                     })
+                    
+                        
+                    
                 }
-                
+               <button className='deleteAll' onClick={deleteAll}><i class="fa fa-trash"></i> &nbsp; DELETE ALL</button>
+               
             </div>
+            
         </div>
     </div>
     </div>
